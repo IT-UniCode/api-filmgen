18b4a0c6d4e8151156c509055fbcd642bf9c3c39
angieene <angel.nerukh@gmail.com>
2023-01-25 17:32:31 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,85 @@
 module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  globals: {
+    beforeAll: true,
+    beforeEach: true,
+    describe: true,
+    expect: true,
+    it: true,
+    jest: true,
+    localStorage: true,
+    chrome: 'readonly',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    camelcase: 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'no-console': 2,
+    'consistent-return': 0,
+    quotes: [2, 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        json5: 'never',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/prefer-default-export': 'off',
+    'no-nested-ternary': 'off',
+  },
+  settings: {
+    "import/resolver": {
+      "typescript": {}
+    },
   },
 };
