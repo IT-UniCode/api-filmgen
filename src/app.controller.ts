f3bd8a72efc0fe7c0ebf4e2221abe8273d2c5915
angieene <angel.nerukh@gmail.com>
2023-01-26 14:05:22 +0200

    husky test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { AppsdaService } from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
