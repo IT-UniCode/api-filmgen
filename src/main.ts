7138b7e142cd9c22011934417ac69eac193fac7c
angieene <angel.nerukh@gmail.com>
2023-02-07 10:55:46 +0200

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { createWriteStream } from 'fs';
+import { get } from 'http';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -6,6 +9,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const serverUrl = 'https://api-filmgen.vercel.app/';
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('v1');
@@ -20,5 +25,30 @@ async function bootstrap() {
   SwaggerModule.setup('v1/docs', app, document);
 
   await app.listen(process.env.PORT || 4444);
+
+  // get the swagger json file (if app is running in development mode)
+  if (process.env.NODE_ENV === 'development') {
+    // write swagger ui files
+    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+    });
+
+    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    });
+
+    get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+      },
+    );
+
+    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    });
+  }
 }
 bootstrap();
