9dc2e8b6c2fc3c47aa9997a7bf8b863e4c3055ff
angieene <angel.nerukh@gmail.com>
2023-02-07 14:13:02 +0200

    try2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
-import { join } from 'path';
-
 import { Module } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { IdValidationPipe } from '../pipes/id-validation.pipes';
@@ -17,10 +14,6 @@ import { MoviesModule } from './movies/movies.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'swagger-static'),
-      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/v1/docs',
-    }),
     GenresModule,
     MoviesModule,
     ScheduleModule.forRoot(),
