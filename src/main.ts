8a5d41f7da91db14352e5036a0088c22434ea755
angieene <angel.nerukh@gmail.com>
2023-02-09 18:12:30 +0200

    favicon

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use('/favicon-*', (_, res) => res.send());
 
   const options = new DocumentBuilder()
     .setTitle('Api Filmgem')
