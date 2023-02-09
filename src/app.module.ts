f8160968561ef62e255029e095aaba58fd4c7cfa
angieene <angel.nerukh@gmail.com>
2023-02-09 17:31:11 +0200

    try 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { IdValidationPipe } from '../pipes/id-validation.pipes';
 
 import { configService } from './config/config.service';
-import { FiltersModule } from './filters/filters.module';
 import { GenresModule } from './genres/genres.module';
 import { MoviesModule } from './movies/movies.module';
 
@@ -13,13 +10,6 @@ import { MoviesModule } from './movies/movies.module';
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     GenresModule,
     MoviesModule,
-    FiltersModule,
-  ],
-  providers: [
-    {
-      provide: APP_PIPE,
-      useClass: IdValidationPipe,
-    },
   ],
 })
 export class AppModule {}
--- src/filters/dto/get-all-filters.response.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-import { GenreEntity } from 'genres/entities/genre.entity';
-import { MaxMinYearResDTO } from 'movies/dto/max-min-year.response.dto';
-
-export class GetAllFiltersRes extends MaxMinYearResDTO {
-  @ApiProperty({ type: GenreEntity, isArray: true })
-  genres: GenreEntity[];
-}
