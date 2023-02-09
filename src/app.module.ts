9bbda0a6af9e352407e96563c99389582a05dea5
angieene <angel.nerukh@gmail.com>
2023-02-09 17:54:20 +0200

    try1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FiltersModule } from 'filters/filters.module';
 
 import { configService } from './config/config.service';
 import { GenresModule } from './genres/genres.module';
@@ -11,7 +10,6 @@ import { MoviesModule } from './movies/movies.module';
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     GenresModule,
     MoviesModule,
-    FiltersModule,
   ],
 })
 export class AppModule {}
